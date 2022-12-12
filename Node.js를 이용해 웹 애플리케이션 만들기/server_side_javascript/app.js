6ab2431ab3f81b2c19268ede5333dabe703e767b
jih3508 <jih3508@ibizsoftware.net>
2022-12-12 13:31:15 +0900

    Express, POST 방식을 이용한 정보의 전달

--- Node.js를 이용해 웹 애플리케이션 만들기/server_side_javascript/app.js
+++ Node.js를 이용해 웹 애플리케이션 만들기/server_side_javascript/app.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.locals.pretty = true
 app.set('view engine', 'jade');
 app.set('views', './views')
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('/form', function(req, res){
+    res.render('form');
+});
+app.get('/form_receiver', function(req, res){
+    var title = req.query.title;
+    var description = req.query.description;
+    console.log(title+', '+description);
+    res.send(title+', '+description);
+});
+app.post('/form_receiver', function(req, res){
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(title+', '+description);
+});
 app.get('/topic/:id', function(req, res){
     var topics = [
         'Javascript is....',
